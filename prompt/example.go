f3dd064885f7b5f42617017de77cbe79c9dfbebb
sunshineplan <sunshineplan@users.noreply.github.com>
2024-03-13 09:31:46 +0800

    prompt: Update (#11)

--- prompt/example.go
+++ prompt/example.go
@@ -5,16 +5,13 @@ import "fmt"
 type Example struct {
 	Input  []string
 	Output string
-	Prefix string
 }
 
-func (ex Example) String() string {
+func (ex Example) Sprint(prefix string) string {
 	switch len(ex.Input) {
 	case 0:
 		return ""
-	case 1:
-		return fmt.Sprintf("Input: %s\nOutput: %s", ex.Input[0], ex.Output)
 	default:
-		return fmt.Sprintf("Input:%s\nOutput: %s", printBatch(ex.Input, ex.Prefix, 0), ex.Output)
+		return fmt.Sprintf("Input:\"\"\"\n%s\"\"\"\nOutput: %s", printBatch(ex.Input, prefix, 0), ex.Output)
 	}
 }
